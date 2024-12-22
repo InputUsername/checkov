57c489e47ff429515d18ff0bd999fe8439930169
Koen Bolhuis <koen.bolhuis@gmail.com>
2024-12-22 13:46:51 +0100

    Add rule for DynamoDB PAY_PER_REQUEST

---
+++ checkov/cloudformation/checks/resource/aws/cost/DynamoDbPayPerRequest.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class DynamoDbPayPerRequest(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure that a DynamoDB table uses pay-per-request billing"
+        id = "CKV_AWS_806"
+        supported_resources = ["AWS::DynamoDB::Table"]
+        categories = [CheckCategories.LOGGING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        properties = conf.get('Properties')
+        if properties is not None:
+            billing_mode = properties.get('BillingMode')
+            if billing_mode == 'PAY_PER_REQUEST':
+                return CheckResult.PASSED
+        return CheckResult.FAILED
+
+    def get_evaluated_keys(self) -> List[str]:
+        return ["Properties/BillingMode"]
+
+
+check = DynamoDbPayPerRequest()
